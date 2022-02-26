fe28639c53315997e494d9b6d5dc969a56fbdb04
Srinivas Jakhar <snjakhar.cse@gmail.com>
2022-02-26 11:14:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import { Navbar } from './components/NavBar/navbar';
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+         <Navbar/>
           <br/>
           <Hompage/>
-      <Footer/>
+         <Footer/>
     </div>
   );
 }
